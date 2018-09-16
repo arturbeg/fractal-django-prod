039fcae8a409cab7c4d0ab6ed785019ff0e7cb2f
Artur Begyan <arturbegyan98@gmail.com>
2018-09-16 16:24:34 +0100

    rest-auth registration

--- fractal/urls.py
+++ fractal/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
 
 	url(r'^rest-auth/', include('rest_auth.urls')),
 
-	# url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+	url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
 
 	url(r'^api/auth/token', obtain_jwt_token),
 
